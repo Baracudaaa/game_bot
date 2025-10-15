3ea5aba05391bcb038e81629ff19b721c9d509ed
Aleksandr Solncev <danil.maslov2000@bk.ru>
2025-10-15 13:56:12 +0300

    add functions /stat, /cancel, positive, negative, numbers, other

---
+++ main.py
@@ -0,0 +1,146 @@
+import random
+
+from aiogram import Bot,Dispatcher, F
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+
+BOT_TOKEN = '8378863385:AAHvtk49agddT8V5u8utvzhHm3zn-heA3MU'
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+# Кол-во попыток в игре
+ATTEMPTS = 5
+
+# Словарь,в котором будут храниться данные пользователя
+user = {
+    'in_game': False,
+    'secret_number': None,
+    'attempts': None,
+    'total_games': 0,
+    'wins': 0
+}
+
+# Функция возвращающая случайное число от 1 до 100
+def get_random_number() -> int:
+    return random.randint(1, 100)
+
+# /start
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(
+        'Привет!\nДавайте сыграем в игру "Угадай число"?\n\n'
+        'Чтобы получить правила игры и список доступных '
+        'команд - отправьте команду /help'
+    )
+
+# /help
+@dp.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Правила игры:\n\nЯ загадываю число от 1 до 100, '
+        f'а вам нужно его угадать\nУ вас есть {ATTEMPTS} '
+        'попыток\n\nДоступные команды:\n/help - правила '
+        'игры и список команд\n/cancel - выйти из игры\n'
+        '/stat - посмотреть статистику\n\nДавай сыграем?'
+    )
+
+# /stat
+@dp.message(Command(commands='stat'))
+async def process_stat_command(message: Message):
+    await message.answer(
+        f'Всего игр сыграно: {user["total_games"]}\n'
+        f'Игр выиграно: {user["wins"]}'
+    )
+
+# /cancel
+@dp.message(Command(commands='cancel'))
+async def process_cancel_command(message: Message):
+    if user['in_game']:
+        user['in_game'] = False
+        await message.answer(
+            'Вы вышли из игры!'
+        )
+    else:
+        await message.answer(
+            'Вы вне игры. Может сыграем разок?'
+        )
+
+# Согласие польхователя
+@dp.message(F.text.lower().in_(['да', 'давай']))
+async def process_positive_answer(message:Message):
+    if not user['in_game']:
+        user['in_game'] = True
+        user['secret_number'] = get_random_number()
+        user['attempts'] = ATTEMPTS
+        await message.answer(
+            'Я загадал число от 1 до 100'
+        )
+    else:
+        await message.answer(
+            'Пока мы играем в игру я могу '
+            'реагировать только на числа от 1 до 100 '
+            'и команды /cancel и /stat'
+        )
+
+# Отказ пользователя
+@dp.message(F.text.lower().in_['не', 'нет'])
+async def process_negative_answer(message: Message):
+    if not user['in_game']:
+        await message.answer(
+            'Жаль :(\n\nЕсли захотите поиграть - просто '
+            'напишите об этом'
+        )
+    else:
+        await message.answer(
+            'Мы же сейчас с вами играем. Присылайте, '
+            'пожалуйста, числа от 1 до 100'
+        )
+
+# 1-100
+@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+async def process_numbers_answer(message: Message):
+    if user['in_game']:
+        if int(message.text) == user['secret_number']:
+            user['in_game'] = False
+            user['total_games'] += 1
+            user['wins'] += 1
+            await message.answer(
+                'Ура!!! Вы угадали число!\n\n'
+                'Может, сыграем еще?'
+            )
+        elif int(message.text) > user['secret_number']:
+            user['attempts'] -= 1
+            await message.answer('Моё число меньше')
+        elif int(message.text) < user['secret_number']:
+            user['attempts'] -= 1
+            await message.answer('Моё число больше')
+
+        if user['attempts'] == 0:
+            user['in_game'] = False
+            user['total_games'] += 1
+            await message.answer(
+                'К сожалению, у вас больше не осталось '
+                'попыток. Вы проиграли :(\n\nМое число '
+                f'было {user["secret_number"]}\n\nДавайте '
+                'сыграем еще?'
+            )
+        else:
+            await message.answer('Мы еще не играем. Хотите сыграть?')
+
+# Остальные ответы
+@dp.message()
+async def process_other_answer(message: Message):
+    if user['in_game']:
+        await message.answer(
+            'Мы же сейчас с вами играем. '
+            'Присылайте, пожалуйста, числа от 1 до 100'
+        )
+    else:
+        await message.answer(
+            'Я довольно ограниченный бот, давайте '
+            'просто сыграем в игру?'
+        )
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
